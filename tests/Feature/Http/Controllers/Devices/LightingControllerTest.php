b6d85fbba6926d40309595a73afe716dfc46ce51
Steven Francoeur-Veillet <steven@cybercat.ca>
2024-10-15 17:26:36 -0400

    feat: SHC-3 - Add tests for Lighting model and controller.
    Introduced Feature tests for the Lighting model and LightingController. The tests cover creating, updating, deleting Lighting models, and validating the response structure of LightingController.

---
+++ tests/Feature/Http/Controllers/Devices/LightingControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Devices\Lighting;
+
+it('returns correct lighting data', function () {
+    $lighting = Lighting::factory()->create();
+
+    expect($lighting->ieee_address)->toBeString()
+        ->and($lighting->friendly_name)->toBeString()
+        ->and($lighting->brightness)->toBeInt()
+        ->and($lighting->energy)->toBeFloat()
+        ->and($lighting->linkquality)->toBeInt()
+        ->and($lighting->power)->toBeFloat()
+        ->and($lighting->state)->toBeString()
+        ->and($lighting->updated_at)->toBeInstanceOf(DateTime::class)
+        ->and($lighting->created_at)->toBeInstanceOf(DateTime::class)
+        ->and($lighting->id)->toBeInt();
+});
