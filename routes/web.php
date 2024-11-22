ee539e73d5edde4c0608d4aeb6466e7b56c8148d
Steven Francoeur-Veillet <steven@cybercat.ca>
2024-11-22 12:20:47 -0500

    feat: SHCA-4 - Implement a feature that restricts user registration.
    Implement a feature that restricts user registration to only those who have received an invitation. Deleted the Welcome.vue component from the resources/js/Pages directory.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
-use App\Enums\PermissionRole;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
 use Inertia\Inertia;
-
-// Switch language
-Route::get('locale/{locale}', function (string $locale) {
-    $locale === 'en' ? $locale = 'fr' : $locale = 'en';
-    Session::Put('locale', $locale);
-
-    return Inertia::location(url()->previous());
-})->name('locale');
 
 Route::get('/', function () {
     if (auth()->check()) {
@@ -21,28 +11,18 @@ Route::get('/', function () {
 
     return Inertia::render('Auth/Login', [
         'canLogin' => Route::has('login'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+        'canRegister' => Route::has('register'),
     ]);
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-    PermissionRole::ROLE->value . ':' .
-    PermissionRole::ADMIN->value,
-])->group(function () {
-    Route::get('/register', function () {
-        return Inertia::render('Auth/Register');
-    })->name('register');
-});
+Route::get('locale/{locale}', function (string $locale) {
+    $locale === 'en' ? $locale = 'fr' : $locale = 'en';
+    Session::Put('locale', $locale);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+    return Inertia::location(url()->previous());
+})->name('locale');
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
