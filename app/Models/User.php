fa9727412895d6408671aaf831ef18c1b236eea0
Steven Francoeur-Veillet <steven@cybercat.ca>
2024-11-22 07:31:25 -0500

    refactor: SHCA-15 - Remove unused imports from User model.
    Removed unnecessary 'Attribute' and 'Storage' imports to clean up the User model. This improves code readability and helps prevent potential future issues related to unused dependencies.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Storage;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
