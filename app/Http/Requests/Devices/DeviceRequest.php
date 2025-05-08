17548fcfc86cbc14a36305948a288e6809e50382
Steven Francoeur-Veillet <steven@cybercat.ca>
2025-05-08 16:16:41 -0400

    feat: enhance DeviceRequest validation rules
    Add conditional validation for 'state' and 'occupiedHeatingSetpoint' based on 'deviceModelClassName' input. Use Enum-based rules for cleaner and more accurate validation.

--- app/Http/Requests/Devices/DeviceRequest.php
+++ app/Http/Requests/Devices/DeviceRequest.php
@@ -2,20 +2,36 @@
 
 namespace App\Http\Requests\Devices;
 
+use App\Enums\DeviceModelClassName;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class DeviceRequest extends FormRequest
 {
     public function rules(): array
     {
-        $startWithCapitalLetter = 'regex:/^[A-Z]/';
-
         return [
             'friendlyName' => 'required|string|max:81',
             'set' => 'required|string|size:4|in:/set',
-            'state' => 'string',
-            'occupiedHeatingSetpoint' => 'numeric|min:15|max:24',
-            'deviceModelClassName' => "required|string|$startWithCapitalLetter",
+            'deviceModelClassName' => [
+                'required',
+                'string',
+                Rule::in(collect(DeviceModelClassName::cases())->map->value->toArray()),
+            ],
+            'state' => [
+                'string',
+                Rule::requiredIf(function () {
+                    return $this->input('deviceModelClassName') === DeviceModelClassName::Lighting->value;
+                }),
+            ],
+            'occupiedHeatingSetpoint' => [
+                'numeric',
+                'min:15',
+                'max:24',
+                Rule::requiredIf(function () {
+                    return $this->input('deviceModelClassName') === DeviceModelClassName::Heating->value;
+                }),
+            ],
         ];
     }
 }
